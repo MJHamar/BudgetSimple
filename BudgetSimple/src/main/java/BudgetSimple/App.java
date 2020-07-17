08c53c58aafab32cf1f8e5d40a7980658d97371c
MJHamar <Miklos.Hamar@warwick.ac.uk>
2020-07-17 16:38:26 +0200

    added empty maven project

---
+++ BudgetSimple/src/main/java/BudgetSimple/App.java
@@ -0,0 +1,13 @@
+package BudgetSimple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
